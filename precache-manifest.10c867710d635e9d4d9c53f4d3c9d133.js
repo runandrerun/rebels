e2c97584ec2e64c6017d644bd0096c4e23d63e17
Andre <the.asantiagojr@gmail.com>
2020-11-16 11:57:49 -0500

    Updates

---
+++ precache-manifest.10c867710d635e9d4d9c53f4d3c9d133.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d6a8e0f2d9fbdb4e9b782721f0d8a5",
+    "url": "/rebels/index.html"
+  },
+  {
+    "revision": "ce28175d61b3bb40f055",
+    "url": "/rebels/static/css/main.9d132118.chunk.css"
+  },
+  {
+    "revision": "1257a30400f3d836e551",
+    "url": "/rebels/static/js/2.b817836c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/rebels/static/js/2.b817836c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce28175d61b3bb40f055",
+    "url": "/rebels/static/js/main.88942a26.chunk.js"
+  },
+  {
+    "revision": "10545a9e46acb9f2944c",
+    "url": "/rebels/static/js/runtime-main.602c3fae.js"
+  },
+  {
+    "revision": "44cf5fd3c02cea7577fe3966e319a662",
+    "url": "/rebels/static/media/apples.44cf5fd3.jpg"
+  },
+  {
+    "revision": "26d5000fa6f8ffeb4af0b57e4c2bef9d",
+    "url": "/rebels/static/media/arrows_down.26d5000f.svg"
+  },
+  {
+    "revision": "50e1186a385c19568945ab3bc8b2c5ab",
+    "url": "/rebels/static/media/bread.50e1186a.jpg"
+  },
+  {
+    "revision": "a62fc03a5c8530ff5fb7ee118a5bbc32",
+    "url": "/rebels/static/media/eggs.a62fc03a.jpg"
+  },
+  {
+    "revision": "c71e5005d7cb1d44637816f0c54749c9",
+    "url": "/rebels/static/media/milk.c71e5005.jpg"
+  },
+  {
+    "revision": "63118c28ac1bafb280fe5dbdb017f909",
+    "url": "/rebels/static/media/misfits-market-w-header-logo.63118c28.svg"
+  },
+  {
+    "revision": "8948d130a90b082b834f49110b793c8b",
+    "url": "/rebels/static/media/shopping-cart.8948d130.svg"
+  },
+  {
+    "revision": "81bf1dc05228119fa9b0930f8ea780a3",
+    "url": "/rebels/static/media/vegetables-1.81bf1dc0.jpg"
+  },
+  {
+    "revision": "2fcf1f4ffaaa042d1396be53c119a59b",
+    "url": "/rebels/static/media/vegetables-2.2fcf1f4f.jpg"
+  },
+  {
+    "revision": "c033b15b064a94c3b1571138f1300fe3",
+    "url": "/rebels/static/media/vegetables-3.c033b15b.jpg"
+  }
+]);
